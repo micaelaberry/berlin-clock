e202f7cbab86d7843c650964dbf3d7593fef3d49
micaela berry <msberry9@gmail.com>
2017-08-30 13:58:12 -0400

    redoing kata

--- src/main/java/BerlinClock.java
+++ src/main/java/BerlinClock.java
@@ -1,21 +1,41 @@
 
 public class BerlinClock {
+	
+	int seconds = 0;
 
-	public String getSeconds(int number) {
-		if (number % 2 == 0) {
-			return "Y";
-		} else
-			return "O";
+	
+	public String seconds() {
+		return evenSeconds()? "Y": "OFF";
 	}
 
-	public String getTopRedTopRow() {
-		return "4";
-	}
-
-	public String getTopRowHours() {
+	public void tick (){
 		
-		for (int number = 0; number <= 5; number++){			
 	}
-	return"5";
+	private boolean evenSeconds(){
+		return true;
+	}
 }
-}
+
+
+
+
+
+
+//	public String getSeconds(int number) {
+//		if (number % 2 == 0) {
+//			return "Y";
+//		} else
+//			return "O";
+//	}
+//
+//	public String getTopRedTopRow() {
+//		return "4";
+//	}
+//
+//	public String getTopRowHours() {
+//		
+//		for (int number = 0; number <= 5; number++){			
+//	}
+//	return"5";
+//}
+
--- src/test/java/BerlinClockTest.java
+++ src/test/java/BerlinClockTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,28 +12,56 @@ import org.junit.Test;
 
 public class BerlinClockTest {
 	
-	BerlinClock berlinClock;
+//	BerlinClock berlinClock;
 	
-	@Before
-	public void setUp(){
-	berlinClock = new BerlinClock();
-	}
-
-	@Test
-	public void everyTwoSecondsTheYellowLampBlinksOff(){
-		assertEquals("Y", berlinClock.getSeconds(0));
-		assertEquals("O", berlinClock.getSeconds(1));
-	}
+//	@Before
+//	public void setUp(){
+//	berlinClock = new BerlinClock();
+//	}
 	
 	@Test
-	public void topRedRowShouldHaveFourLamps(){
-		assertEquals("4", berlinClock.getTopRedTopRow());	
+	public void  shouldBeenYellowOnEven(){
+		BerlinClock underTest = new BerlinClock();
+		String seconds = underTest.seconds();
+		assertThat(seconds, is("Y"));
 	}
 	
 	@Test
-	public void topRedRowShouldReturn5Hours(){
-		assertEquals("5", berlinClock.getTopRowHours());
+	public void shouldBeOffOnOddSeconds(){
+		BerlinClock underTest = new BerlinClock();
+		underTest.tick();
+		String seconds = underTest.seconds();
+		assertThat(seconds, is("OFF"));
 	}
 	
-
+	@Test
+	public void shouldBeOnEveryTwoSeconds(){
+		BerlinClock underTest = new BerlinClock();
+		underTest.tick();
+		String seconds = underTest.seconds();
+		assertThat(seconds, is("Y"));
+	}
+	
+	
+	
+	
+	
 }
+//	@Test
+//	public void everyTwoSecondsTheYellowLampBlinksOff(){
+//		assertEquals("Y", berlinClock.getSeconds(0));
+//		assertEquals("O", berlinClock.getSeconds(1));
+//	}
+//	
+//	@Test
+//	public void topRedRowShouldHaveFourLamps(){
+//		assertEquals("4", berlinClock.getTopRedTopRow());	
+//	}
+//	
+//	@Test
+//	public void topRedRowShouldReturn5Hours(){
+//		assertEquals("5", berlinClock.getTopRowHours());
+//	}
+	
+
+
